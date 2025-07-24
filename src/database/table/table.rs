3233fa4d8e609cbfdb0e5c205395ff73a377ca6b
FAZuH <fazuhhh@proton.me>
2025-07-24 15:49:54 +0700

    add Table.select_all()

--- src/database/table/table.rs
+++ src/database/table/table.rs
@@ -4,6 +4,7 @@ use async_trait::async_trait;
 pub trait Table<T, ID> {
     async fn create_table(&self) -> anyhow::Result<()>;
     async fn drop_table(&self) -> anyhow::Result<()>;
+    async fn select_all(&self) -> anyhow::Result<Vec<T>>;
     async fn delete_all(&self) -> anyhow::Result<()>;
     async fn insert(&self, model: &T) -> anyhow::Result<()>;
     async fn select(&self, id: &ID) -> anyhow::Result<T>;
