8893371b7d34b3d63a1bc426c60d6c4e2e392fb2
FAZuH <fazuhhh@proton.me>
2025-07-28 14:46:31 +0700

    remove action module

--- src/action/action.rs
+++
@@ -1,9 +0,0 @@
-use crate::event::manga_update_event::MangaUpdateEvent;
-use async_trait::async_trait;
-use std::any::Any;
-
-#[async_trait]
-pub trait Action: Send + Sync {
-    async fn run(&self, event: &MangaUpdateEvent) -> anyhow::Result<()>;
-    fn as_any(&self) -> &dyn Any;
-}
