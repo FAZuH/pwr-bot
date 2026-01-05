d7941dd73813fa62c00f292e147e87c9522b50e6
FAZuH <mail@fazuh.com>
2026-01-05 17:23:35 +0700

    feat: Implement VoiceStateSubscriber

--- src/subscriber/voice_state_subscriber.rs
+++ src/subscriber/voice_state_subscriber.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use chrono::Utc;
 
+use crate::database::model::VoiceSessionsModel;
 use crate::event::VoiceStateEvent;
 use crate::service::Services;
 use crate::subscriber::Subscriber;
@@ -15,7 +17,35 @@ impl VoiceStateSubscriber {
         Self { services }
     }
     pub async fn callback(&self, event: VoiceStateEvent) -> Result<()> {
-        todo!()
+        let new = event.new;
+
+        let join_time = match new.request_to_speak_timestamp {
+            Some(res) => res.to_utc(),
+            None => return Ok(()),
+        };
+        let user_id = new.user_id.get();
+        let guild_id = match new.guild_id {
+            Some(res) => res.get(),
+            None => return Ok(()),
+        };
+        let channel_id = match new.channel_id {
+            Some(res) => res.get(),
+            None => return Ok(()),
+        };
+        let leave_time = Utc::now();
+
+        let model = VoiceSessionsModel {
+            user_id,
+            guild_id,
+            channel_id,
+            join_time,
+            leave_time,
+            ..Default::default()
+        };
+
+        self.services.voice_tracking.insert(&model).await?;
+
+        Ok(())
     }
 }
 
